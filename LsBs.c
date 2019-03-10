6995c2707e47c21eb4487383729e0e03d558e5a5
Akshita <31439578+akshusharma1526@users.noreply.github.com>
2019-03-10 10:01:01 +0530

    Add files via upload

---
+++ LsBs.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,n,a[10],ls,bs;
+	for(i=0;i<10;i++)
+	scanf("%d",&a[i]);
+	printf("Enter element to be searched:\n");
+	scanf("%d",&n);
+    ls=linear(a,n);
+    if(ls)
+    printf("Element found at %d\n", ls);
+    else
+    printf("Element not found");
+    bs=binary(a,n);
+    if(bs)
+    printf("Element found at %d", bs);
+    else
+    printf("Element not found");
+	return 0;
+}
+int linear(int a[],int n)
+{
+	int i;
+		for(i=0;i<10;i++)
+	{
+		if(a[i]==n)
+		return i+1;
+	}
+	return 0;
+}
+int binary(int a[],int n)
+{
+	int start=0, end=9;             
+	int mid=(start+end)/2;
+	while(start<=end)
+	{
+		if(a[mid]==n)
+		return mid+1;
+		else if(a[mid]<n)
+		start=mid+1;
+		else
+		end=mid-1;
+		
+	mid=(start+end)/2;
+	}
+	
+}
+
